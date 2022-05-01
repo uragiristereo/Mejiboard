204374ef168f2dc2eaef235b23651186e9a71c73
Agung Watanabe <raava@outlook.co.id>
2022-05-01 13:09:28 +0700

    improve: use generic function for state update mapper

--- app-alpha/src/main/java/com/github/uragiristereo/mejiboard/presentation/common/mapper/StateUpdateMapper.kt
+++ app-alpha/src/main/java/com/github/uragiristereo/mejiboard/presentation/common/mapper/StateUpdateMapper.kt
@@ -1,10 +1,8 @@
 package com.github.uragiristereo.mejiboard.presentation.common.mapper
 
 import androidx.compose.runtime.MutableState
-import com.github.uragiristereo.mejiboard.presentation.settings.core.SettingsState
 
 
-@JvmName("updateSettingsState")
-inline fun MutableState<SettingsState>.update(function: (SettingsState) -> SettingsState) {
+inline fun <T> MutableState<T>.update(function: (T) -> T) {
     value = function(value)
 }
